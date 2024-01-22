ed370d0bd965b31d6d87629337f24c1d17d1b6a0
nrhsnani <75931747+nrhsnani@users.noreply.github.com>
2024-01-22 08:36:55 +0000

    update Dockerfile

--- testgeospatial.py
+++ testgeospatial.py
@@ -5,6 +5,6 @@ def read_shapefile(file_path):
     return df
 
 # Example usage:
-shapefile_path = 'CEN_FDP_BND_SHAPEFILE/CEN_FDP_BND.shp'
+shapefile_path = 'CEN_FDP_BND.shp'
 result_df = read_shapefile(shapefile_path)
 print(result_df.head())
