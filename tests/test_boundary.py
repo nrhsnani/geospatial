52726a8ba36d7419ad13f09d70db7ed9097e5df8
Afifah Azman <53687015+fyfaazman@users.noreply.github.com>
2024-01-29 10:35:57 +0800

    Update and rename testboundary.py to test_boundary.py

---
+++ tests/test_boundary.py
@@ -0,0 +1,34 @@
+import geopandas as gpd
+
+def read_shapefile(file_path):
+    df = gpd.read_file(file_path)
+    return df
+
+def find_boundary(point, polygon_gdf):
+    point_geom = Point(point)
+
+    for index, row in polygon_gdf.iterrows():
+        if point_geom.within(row['geometry']):
+            return row['BND_ID']
+    # If the point is not inside any polygon, return None or any other appropriate value
+    return 'N/A'
+
+# Example usage:
+cust_input = pd.read_csv('dataCust.csv')
+
+shpfile_path = 'dataSHP.shp'
+polygon_gdf = read_shapefile(shpfile_path)
+
+result = []
+
+for index, row in cust_input.iterrows():
+    x_longitude = row['Longitude']
+    y_latitude = row['Latitude']
+    point = Point(x_longitude, y_latitude)
+    
+    # Find the boundary_id for the point
+    bnd_id = find_boundary(point, polygon_gdf)
+    result.append(bnd_id)
+
+cust_input['Boundary_Overlap'] = result
+# cust_input
